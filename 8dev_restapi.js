a79d597f59e17d07367c47a9ee45fe00c6dad3a3
Nedas <nedas@8devices.com>
2018-07-18 12:43:25 +0300

    Automatically resolve local IP address for notification callback registration (#49)
    * Added node module which helps retrieving network IP address.
    * This IP address is used in notification callback registration in order to show REST server where to send notifications.

--- 8dev_restapi.js
+++ 8dev_restapi.js
@@ -4,6 +4,7 @@ const EventEmitter = require('events');
 const rest = require('node-rest-client');
 const express = require('express');
 const parser = require('body-parser');
+const ip = require('ip');
 
 class Endpoint extends EventEmitter {
   constructor(service, id) {
@@ -137,6 +138,7 @@ class Service extends EventEmitter {
       port: 5728,
     };
     this.configure(opts);
+    this.ipAddress = ip.address();
     this.client = new rest.Client();
     this.endpoints = [];
     this.addTlvSerializer();
@@ -199,7 +201,7 @@ class Service extends EventEmitter {
   registerNotificationCallback() {
     return new Promise((fulfill, reject) => {
       const data = {
-        url: `http://localhost:${this.config.port}/notification`,
+        url: `http://${this.ipAddress}:${this.config.port}/notification`,
         headers: {},
       };
       const type = 'application/json';
