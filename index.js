09590273297d0c59e0aca7fc8edaf7713aa3d02c
mrNedas <nedas@8devices.com>
2018-11-30 14:43:25 +0000

    Removed emuletor and 8dev_sensors.js

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 
 
 const api = require('./8dev_restapi.js');
-const emulate = require('./8dev_emulate/nodes/index.js');
 const lwm2m = require('./lwm2m/index.js');
 
 module.exports.Service = api.Service;
 module.exports.Device = api.Device;
-module.exports.Emulate = emulate;
 module.exports.Lwm2m = lwm2m;
